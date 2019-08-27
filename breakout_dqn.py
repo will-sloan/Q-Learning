8f18508b7ae2425d43f88c6a9c96c7a628f4a273
will-sloan <willis5552@gmail.com>
2019-08-27 19:51:40 -0400

    small changes, still dont work

--- breakout_dqn.py
+++ breakout_dqn.py
@@ -7,8 +7,8 @@ from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout
 from tqdm import tqdm
 import numpy as np
 import random
-
-
+import tensorflow as tf
+print(tf.__version__)
 env = gym.make('Breakout-v0')
 
 env.reset()
@@ -44,7 +44,7 @@ class DQNAgent:
             model.add(Conv2D(32, (3,3), activation='relu'))
             model.add(Dropout(0.2))
             model.add(Flatten())
-            model.add(Dense(units=64, kernel_initializer='RandomNormal', activation='relu'))
+            model.add(Dense(64))
             model.add(Dense(units=env.action_space.n, activation='linear'))
             model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
         return model
@@ -61,34 +61,33 @@ class DQNAgent:
         minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
 
         current_states = np.array([transition[0] for transition in minibatch])/255
-        current_q_list = self.model.predict(current_states)
+        current_qs_list = self.model.predict(current_states)
         new_current_states = np.array([transition[3] for transition in minibatch])/255
-        future_q_list = self.backup_model.predict(new_current_states)
+        future_qs_list = self.backup_model.predict(new_current_states)
 
         X = []
-        y= []
-
+        y = []
 
         for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
             if not done:
-                max_future_q = np.max(future_q_list)
-                new_q = max_future_q * DISCOUNT + reward
+                max_future_q = np.max(future_qs_list)
+                new_q = reward + DISCOUNT * max_future_q
 
             else:
-                new_q = reward
+                new_q  = reward
 
-            current_qs = current_q_list[index]
+            current_qs = current_qs_list[index]
             current_qs[action] = new_q
 
             X.append(current_state)
             y.append(current_qs)
-        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False)
+        self.model.fit(np.array(X)/255, np.array(y), batch_size= MINIBATCH_SIZE, verbose=0, shuffle=False)
 
         if terminal_state:
-            self.backup_update_counter +=1
+            self.backup_update_counter += 1
 
         if self.backup_update_counter > UPDATE_BACKUP_EVERY:
-            self.backup_update_counter.set_weights(self.model.get_weights())
+            self.backup_model.set_weights(self.model.get_weights())
             self.backup_update_counter = 0
 
 REPLAY_MEMORY_SIZE = 50_000
@@ -105,13 +104,14 @@ AGGREGATE_STATS_EVERY = 100
 ep_rewards = [-200]
 MIN_EPSILON = 0.01
 agent = DQNAgent()
+aggr_ep_rewards = {'ep': [], 'avg': [], 'min': [], 'max' : []}
 
 for episode in tqdm(range(1, EPISODES+1), ascii=True, unit='episode'):
     current_state = env.reset()
     episode_reward = 0
     done = False
     while not done:
-        if SHOW and not episode % AGGREGATE_STATS_EVERY:
+        if SHOW:
             env.render()
 
         if np.random.random() > epsilon:
@@ -120,25 +120,12 @@ for episode in tqdm(range(1, EPISODES+1), ascii=True, unit='episode'):
             action = np.random.randint(0, env.action_space.n)
 
         new_state, reward, done, _ = env.step(action)
+        print(f"done is {done}")
         episode_reward += reward
         agent.update_replay_memory((current_state, action, reward, new_state, done))
         agent.train(done)
         current_state = new_state
 
-
-    ep_rewards.append(episode_reward)
-    if not episode % AGGREGATE_STATS_EVERY:
-        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        aggr_ep_rewards['ep'].append(episode)
-        aggr_ep_rewards['avg'].append(sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:]))
-        aggr_ep_rewards['min'].append(min(ep_rewards[-AGGREGATE_STATS_EVERY:]))
-        aggr_ep_rewards['max'].append(max(ep_rewards[-AGGREGATE_STATS_EVERY:]))
-
-    if epsilon > MIN_EPSILON:
-        epsilon *= EPSILON_DECAY
-        epsilon = max(MIN_EPSILON, epsilon)
 
 plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label='avg')
 plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label='min')
